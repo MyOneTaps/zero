02a54a299601d65a1d0941c375a6a022d1998d6d
dwenegar <simone.livieri@gmail.com>
2021-05-09 07:26:27 +0900

    fonts: update fonts

--- fonts.lua
+++ fonts.lua
@@ -1,3 +1,4 @@
-ZERO_UI_FONT = [[Interface\AddOns\Zero\media\fonts\Roboto.ttf]]
-ZERO_DEFAULT_FONT = [[Interface\AddOns\Zero\media\fonts\Roboto.ttf]]
-ZERO_DAMAGE_FONT = [[Interface\AddOns\Zero\media\fonts\PasseroOne.ttf]]
+ZERO_UI_FONT = [[Interface\AddOns\Zero\media\fonts\Accidental Presidency.ttf]]
+ZERO_DEFAULT_FONT = [[Interface\AddOns\Zero\media\fonts\Accidental Presidency.ttf]]
+ZERO_DAMAGE_FONT = [[Interface\AddOns\Zero\media\fonts\Bangers.ttf]]
+ZERO_CHAT_FONT = [[Interface\AddOns\Zero\media\fonts\Roboto.ttf]]
