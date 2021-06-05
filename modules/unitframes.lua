3ca0a77790743d30ec4a6a98753c19317d58412e
dwenegar <simone.livieri@gmail.com>
2021-06-05 09:04:39 +0900

    auto hide player/target frames

--- modules/unitframes.lua
+++ modules/unitframes.lua
@@ -2,30 +2,65 @@ local _, Zero = ...
 
 local module = Zero.Module('UnitFrames')
 
-local function SetUnitFrame(frame, x, y)
-  frame:StartMoving()
-  frame:SetUserPlaced(true)
+local function UpdatePlayerFrameVisibility()
+  if InCombatLockdown() then return end
+
+  local atMaxHealth = UnitHealth('player') == UnitHealthMax('player')
+  local hasTarget = UnitExists('target')
+
+  if hasTarget or not atMaxHealth then
+    PlayerFrame:Show()
+  else
+    PlayerFrame:Hide()
+  end
+end
+
+local function CreateDragFrame(name, width, height)
+
+  local frame = CreateFrame('Frame', name, UIParent)
+  frame:SetMovable(true)
+  frame:EnableMouse(true)
+  frame:SetPoint('CENTER')
+  frame:SetSize(width, height)
+
+  -- Hide it
+  frame:Hide()
+
+  return frame
+end
+
+local function SetUnitFrame(frame, dragFrame, x, y)
+  dragFrame:ClearAllPoints()
+  dragFrame:SetPoint('BOTTOM', x, y)
+
+  frame:SetMovable(true)
   frame:ClearAllPoints()
-  frame:SetPoint('BOTTOM', x, y)
-  frame:StopMovingOrSizing()
+  frame:SetPoint('CENTER', dragFrame, 'CENTER')
+  frame:SetUserPlaced(true)
+  frame:SetMovable(false)
 end
 
-local function UpdatePlayerFrame()
-  SetUnitFrame(PlayerFrame, -265, 250)
+function module:UpdateUnitFrames()
+  SetUnitFrame(PlayerFrame, self.playerDragFrame, -265, 250)
+  SetUnitFrame(TargetFrame, self.targetDragFrame, 265, 250)
 end
 
-local function UpdateUnitFrames()
-  SetUnitFrame(PlayerFrame, -265, 250)
-  SetUnitFrame(TargetFrame, 265, 250)
-  -- TargetFrame.buffsOnTop = true
+function module:OnLoad()
+  self.playerDragFrame = CreateDragFrame('Zero_Player_DragFrame', 205, 90)
+  self.targetDragFrame = CreateDragFrame('Zero_Target_DragFrame', 205, 90)
+  self:UpdateUnitFrames()
 end
 
 function module:OnPlayerLogin()
-  self:SafeInvoke(UpdateUnitFrames)
-  hooksecurefunc('PlayerFrame_UpdateArt', function()
-    self:SafeInvoke(UpdatePlayerFrame)
-  end)
-  hooksecurefunc('PlayerFrame_SequenceFinished', function()
-    self:SafeInvoke(UpdatePlayerFrame)
-  end)
+  self:SafeInvoke('UpdateUnitFrames')
+
+  -- self:RegisterEvent('UNIT_HEALTH', UpdatePlayerFrameVisibility)
+  self:RegisterEvent('PLAYER_TARGET_CHANGED', UpdatePlayerFrameVisibility)
+  self:RegisterEvent('UNIT_POWER_UPDATE', UpdatePlayerFrameVisibility)
+  self:RegisterEvent('PLAYER_REGEN_DISABLED', UpdatePlayerFrameVisibility)
+  self:RegisterEvent('PLAYER_REGEN_ENABLED', UpdatePlayerFrameVisibility)
+  self:RegisterEvent('UNIT_EXITED_VEHICLE', UpdatePlayerFrameVisibility)
+  self:RegisterEvent('UNIT_ENTERED_VEHICLE', UpdatePlayerFrameVisibility)
+
+  UpdatePlayerFrameVisibility()
 end
