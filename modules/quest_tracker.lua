0a5fe8d15e85ee2dabf137c2513c3697765afb57
dwenegar <simone.livieri@gmail.com>
2021-06-19 13:18:16 +0900

    update the quest tracker using a timer

--- modules/quest_tracker.lua
+++ modules/quest_tracker.lua
@@ -2,17 +2,27 @@ local _, Zero = ...
 
 local module = Zero.Module('QuestTracker')
 
-function module:OnPlayerLogin()
-  self:RegisterEvent('PLAYER_ENTERING_WORLD', function()
-    local inInstance, instanceType = IsInInstance()
-    if inInstance and instanceType ~= 'none' then
-      if not ObjectiveTrackerFrame.collapsed then
-        ObjectiveTracker_Collapse()
-      end
-    elseif not inInstance then
-      if ObjectiveTrackerFrame.collapsed then
-        ObjectiveTracker_Collapse()
-      end
+local function CheckObjectiveTrackerFrame()
+  if ObjectiveTrackerFrame.collapsed then
+    if not IsInInstance() then
+      ObjectiveTracker_Expand()
+    else
+      C_Timer.After(1, CheckObjectiveTrackerFrame)
     end
-  end)
+  end
+end
+
+function module:PLAYER_ENTERING_WORLD()
+  local inInstance, instanceType = IsInInstance()
+  if inInstance and instanceType ~= 'none' then
+    if not ObjectiveTrackerFrame.collapsed then
+      ObjectiveTracker_Collapse()
+    end
+  else
+    C_Timer.After(1, CheckObjectiveTrackerFrame)
+  end
+end
+
+function module:OnPlayerLogin()
+  self:RegisterEvent('PLAYER_ENTERING_WORLD')
 end
