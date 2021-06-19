e85f8be09d7a71d354a23be5654f9f066ee83908
dwenegar <simone.livieri@gmail.com>
2021-06-19 13:15:52 +0900

    fix delete popup handling

--- modules/delete.lua
+++ modules/delete.lua
@@ -2,16 +2,13 @@ local _, Zero = ...
 
 local module = Zero.Module('Delete')
 
-local function ConfirmDelete(which)
+local function ConfirmDelete(_, which)
   if which == 'DELETE_GOOD_ITEM' then
     for index = 1, 4 do
       local frame = _G['StaticPopup' .. index];
       if frame:IsShown() and frame.which == which then
-        local editBox = _G['StaticPopup' .. index .. 'EditBox']
-        local button1 = _G['StaticPopup' .. index .. 'Button1']
-
-        editBox:Hide()
-        button1:Enable()
+        _G['StaticPopup' .. index .. 'EditBox']:Hide()
+        _G['StaticPopup' .. index .. 'Button1']:Enable()
       end
     end
   end
